3ae8dcb5d71d0acf4256f7fd6607697875468c37
Gilbert Niyonkuru <gilbert.niyonkuru24ba@keplercollege.ac.rw>
2025-07-07 18:52:23 +0200

    Update explain_helper.py

--- explain_helper.py
+++ explain_helper.py
@@ -1,6 +1,8 @@
 import os
 from openai import OpenAI
+from dotenv import load_dotenv
 
+load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 SYSTEM = """You are a biblical scholar assistant.
